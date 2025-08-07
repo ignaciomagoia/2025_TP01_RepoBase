f1459e8a2b981cf623a416091c20c4fda9e36e59
Ignacio Magoia <ignaciomagoiia@hotmail.com>
2025-08-07 11:32:29 -0300

    feat: agregue funcion obtenerFechaActual()

--- src/app.js
+++ src/app.js
@@ -5,9 +5,21 @@ function saludar() {
 
 saludar();
 
+
+//funcion para saludar con el nombre
+
 function saludarConNombre(nombre) {
     console.log(`Hola, ${nombre}`);
 }
 
 saludar();
 saludarConNombre("Ignacio");
+
+//funcion para obtener la fecha actual
+
+function obtenerFechaActual() {
+    const fecha = new Date();
+    return fecha.toLocaleDateString();
+}
+
+console.log("Fecha actual:", obtenerFechaActual());
